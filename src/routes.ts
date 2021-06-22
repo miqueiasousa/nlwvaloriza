b9ae3da0fc61d369975949164165ceb748b2af92
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-22 18:57:49 -0300

    Create route to create user

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express'
 
+import { CreateUserController } from './controller/CreateUserController'
+
 const router = Router()
+
+const createUserController = new CreateUserController()
+
+router.post('/users', createUserController.handle)
 
 export { router }
