806f8ba44eb22853429bba90da39a35528672f89
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-22 19:00:17 -0300

    Use json parser middleware

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import './database/connect'
 const app = express()
 const PORT = Number(process.env.PORT) || 5000
 
+app.use(express.json())
+
 app.use(router)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
