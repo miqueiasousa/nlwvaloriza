055b4470339181ec7906bcae1e0df882a7e173b4
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-22 18:56:55 -0300

    Import routes file into index.ts and use to list routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata'
 import express from 'express'
 
+import { router } from './routes'
+
 import './database/connect'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 5000
+
+app.use(router)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
