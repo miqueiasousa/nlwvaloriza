90f778a11110643ea83e69e887618d7b928ffa84
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-23 18:57:32 -0300

    Create service CreateTagService

---
+++ src/service/CreateTagService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from 'typeorm'
+
+import { TagsRepository } from '../repository/TagsRepository'
+
+export class CreateTagService {
+  async execute (name: string) {
+    const tagsRepository = getCustomRepository(TagsRepository)
+
+    const isTagAlreadyExists = !!await tagsRepository.findOne({ name })
+
+    if (isTagAlreadyExists) {
+      throw new Error('Tag already exists')
+    }
+
+    const tag = tagsRepository.createWithUUID({ name })
+
+    await tagsRepository.save(tag)
+
+    return tag
+  }
+}
