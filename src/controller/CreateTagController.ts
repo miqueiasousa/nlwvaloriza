a7d41d4ddcdb548ff6020415c8624bbe15daf5d0
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-23 18:58:19 -0300

    Create controller CreateTagController

---
+++ src/controller/CreateTagController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+
+import { CreateTagService } from '../service/CreateTagService'
+
+export class CreateTagController {
+  async handle (req: Request, res: Response) {
+    const { name } = req.body
+
+    const createTagService = new CreateTagService()
+
+    const tag = await createTagService.execute(name)
+
+    return res.status(201).json(tag)
+  }
+}
