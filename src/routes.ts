3e1e12e89f9593cc3851fb5e69c15cc37b579046
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-23 19:00:22 -0300

    Use ensureAdmin middleware on create tag route

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 
 import { CreateTagController } from './controller/CreateTagController'
 import { CreateUserController } from './controller/CreateUserController'
+import { ensureAdmin } from './middleware/ensureAdmin'
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 
-router.post('/users', createUserController.handle)
+router.post('/users', ensureAdmin, createUserController.handle)
 router.post('/tags', createTagController.handle)
 
 export { router }
