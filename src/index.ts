9c7541c3482f626976523f6433e24d8560e828bd
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-24 15:35:41 -0300

    Move error handler middleware to own file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata'
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
 import 'express-async-errors'
 
 import { router } from './routes'
+import { errorHandler } from './middleware/errorHandler'
 
 import './database/connect'
 
@@ -13,15 +14,6 @@ app.use(express.json())
 
 app.use(router)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return res.status(400).json({ error: err.message })
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error'
-  })
-})
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express'
+
+export function errorHandler (err: Error, req: Request, res: Response, next: NextFunction) {
+  if (err instanceof Error) {
+    return res.status(400).json({ error: err.message })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+}
