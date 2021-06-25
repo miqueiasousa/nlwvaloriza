cf2b2b1e9b8b4bd157f979287f970682b518abd0
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-24 21:09:10 -0300

    Create repository ComplimentsRepository

---
+++ src/repository/ComplimentsRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository, DeepPartial } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+import { Compliment } from '../entity/Compliment'
+
+@EntityRepository(Compliment)
+export class ComplimentsRepository extends Repository<Compliment> {
+  createWithUUID (entity: DeepPartial<Compliment>) {
+    entity.id = uuid()
+
+    return this.create(entity)
+  }
+}
