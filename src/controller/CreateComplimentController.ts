7d8012ee342d826b4a205339e8273009193d9d11
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-25 18:34:20 -0300

    Set user_sender on controller instead of receive via body

--- src/controller/CreateComplimentController.ts
+++ src/controller/CreateComplimentController.ts
@@ -4,11 +4,16 @@ import { CreateComplimentService } from '../service/CreateComplimentService'
 
 export class CreateComplimentController {
   async handle (req: Request, res: Response) {
-    const { tag_id, user_sender, user_receiver, message } = req.body
+    const { tag_id, user_receiver, message } = req.body
 
     const createComplimentService = new CreateComplimentService()
 
-    const compliment = await createComplimentService.execute({ tag_id, user_sender, user_receiver, message })
+    const compliment = await createComplimentService.execute({
+      tag_id,
+      user_sender: req.userId,
+      user_receiver,
+      message
+    })
 
     return res.status(201).json(compliment)
   }
