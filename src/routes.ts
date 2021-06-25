c0f131e0d3a7ea71fe021608802a7d234a8c08a8
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-25 16:09:43 -0300

    Create route to create compliment

--- src/routes.ts
+++ src/routes.ts
@@ -4,15 +4,18 @@ import { CreateTagController } from './controller/CreateTagController'
 import { CreateUserController } from './controller/CreateUserController'
 import { AuthenticateUserController } from './controller/AuthenticateUserController'
 import { ensureAdmin } from './middleware/ensureAdmin'
+import { CreateComplimentController } from './controller/CreateComplimentController'
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post('/', authenticateUserController.handle)
 router.post('/users', ensureAdmin, createUserController.handle)
 router.post('/tags', createTagController.handle)
+router.post('/compliments', createComplimentController.handle)
 
 export { router }
