d2b7a2d98017170c3afab6ab666c6e4d745e69c8
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-25 17:43:30 -0300

    Use ensureAuthenticated middleware on routes were is necessary authentication

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { CreateUserController } from './controller/CreateUserController'
 import { AuthenticateUserController } from './controller/AuthenticateUserController'
 import { ensureAdmin } from './middleware/ensureAdmin'
 import { CreateComplimentController } from './controller/CreateComplimentController'
+import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
 
@@ -14,8 +15,8 @@ const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
 
 router.post('/', authenticateUserController.handle)
-router.post('/users', ensureAdmin, createUserController.handle)
-router.post('/tags', ensureAdmin, createTagController.handle)
-router.post('/compliments', createComplimentController.handle)
+router.post('/users', ensureAuthenticated, ensureAdmin, createUserController.handle)
+router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
+router.post('/compliments', ensureAuthenticated, createComplimentController.handle)
 
 export { router }
