25bac0a0308195d4f529fb84e0b6a73b2136e861
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-26 01:12:18 -0300

    Create controller ListTagsController

---
+++ src/controller/ListTagsController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+
+import { ListTagsService } from '../service/ListTagsService'
+
+export class ListTagsController {
+  async handle (req: Request, res: Response) {
+    const listTagsService = new ListTagsService()
+
+    const tags = await listTagsService.execute()
+
+    return res.status(200).json(tags)
+  }
+}
