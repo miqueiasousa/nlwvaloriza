93f0bcf013ff168d2989554ff2ee7f69de5d0fe4
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-26 20:53:52 -0300

    Create route to list compliments received for the authenticated user

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateUserController } from './controller/AuthenticateUserControl
 import { CreateComplimentController } from './controller/CreateComplimentController'
 import { ListTagsController } from './controller/ListTagsController'
 import { ListUserSendComplimentsController } from './controller/ListUserSendComplimentsController'
+import { ListUserReceiveComplimentsController } from './controller/ListUserReceiveComplimentsController'
 import { ensureAdmin } from './middleware/ensureAdmin'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
@@ -17,6 +18,7 @@ const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
 const listTagsController = new ListTagsController()
 const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
 
 router.post('/', authenticateUserController.handle)
 
@@ -24,6 +26,7 @@ router.get('/tags', listTagsController.handle)
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
 
 router.get('/user/compliments/send', ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get('/user/compliments/receive', ensureAuthenticated, listUserReceiveComplimentsController.handle)
 
 router.use(ensureAuthenticated)
 
