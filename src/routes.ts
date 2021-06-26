f05f01c3bd6ca607cd104dd46b00c98341456b3c
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-26 01:12:41 -0300

    Create route to list tags

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from './controller/CreateTagController'
 import { CreateUserController } from './controller/CreateUserController'
 import { AuthenticateUserController } from './controller/AuthenticateUserController'
 import { CreateComplimentController } from './controller/CreateComplimentController'
+import { ListTagsController } from './controller/ListTagsController'
 import { ensureAdmin } from './middleware/ensureAdmin'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
@@ -13,13 +14,16 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listTagsController = new ListTagsController()
 
 router.post('/', authenticateUserController.handle)
+
+router.get('/tags', listTagsController.handle)
+router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
 
 router.use(ensureAuthenticated)
 
 router.post('/users', ensureAdmin, createUserController.handle)
-router.post('/tags', ensureAdmin, createTagController.handle)
 router.post('/compliments', createComplimentController.handle)
 
 export { router }
