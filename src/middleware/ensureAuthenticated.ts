163640e151db64d3265d000b479d9c33cc4942b4
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-06-28 15:52:06 -0300

    Create interface ITokenPayload

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 
+interface ITokenPayload {
+  sub: string
+  exp: number
+}
+
 export function ensureAuthenticated (req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers
 
@@ -11,9 +16,9 @@ export function ensureAuthenticated (req: Request, res: Response, next: NextFunc
   const token = authorization.split(' ')[1]
 
   try {
-    const { sub } = verify(token, 'DOGECOIN')
+    const { sub } = verify(token, 'DOGECOIN') as ITokenPayload
 
-    req.userId = sub as string
+    req.userId = sub
 
     return next()
   } catch (error) {
