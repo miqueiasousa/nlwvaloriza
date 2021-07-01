6e9b1928fcce4a022205edbb8b7869890f6c7def
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-07-01 20:47:40 -0300

    Create validation to verify if tag exists

--- src/service/CreateComplimentService.ts
+++ src/service/CreateComplimentService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from 'typeorm'
 
 import { ComplimentsRepository } from '../repository/ComplimentsRepository'
+import { TagsRepository } from '../repository/TagsRepository'
 import { UsersRepository } from '../repository/UsersRepository'
 import { BadRequest, NotFound } from '../core/ApiError'
 
@@ -14,15 +15,17 @@ interface IComplimentRequest {
 export class CreateComplimentService {
   async execute ({ tag_id, user_sender, user_receiver, message }: IComplimentRequest) {
     const complimentsRepository = getCustomRepository(ComplimentsRepository)
+    const tagsRepository = getCustomRepository(TagsRepository)
     const usersRepository = getCustomRepository(UsersRepository)
 
     if (user_sender === user_receiver) {
       throw new BadRequest()
     }
 
+    const isTagExists = !!await tagsRepository.findOne(tag_id)
     const isUserReceiverExists = !!await usersRepository.findOne(user_receiver)
 
-    if (!isUserReceiverExists) {
+    if (!isTagExists || !isUserReceiverExists) {
       throw new NotFound()
     }
 
