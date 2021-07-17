6cbdede05d1177d75c789b0754f718facb4872a0
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-07-17 14:39:16 -0300

    Delete repositories

--- src/repository/ComplimentsRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-
-import { Compliment } from '../entity/Compliment'
-
-@EntityRepository(Compliment)
-export class ComplimentsRepository extends Repository<Compliment> {}
--- src/repository/TagsRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-
-import { Tag } from '../entity/Tag'
-
-@EntityRepository(Tag)
-export class TagsRepository extends Repository<Tag> {}
